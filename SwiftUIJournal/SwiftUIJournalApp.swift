c87874507655db7cdcafcb20b65f91ab2e096c56
Jonas Sjögrenj <jonas.sjogren@iths.se>
2022-12-22 09:01:55 +0100

    Initial Commit

---
+++ SwiftUIJournal/SwiftUIJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIJournalApp.swift
+//  SwiftUIJournal
+//
+//  Created by Jonas Sjögren on 2022-12-22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
